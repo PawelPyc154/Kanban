641e0e08078b78a62004cbff9ebefce1a8da4dd7
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-07-29 22:02:08 +0200

    add global style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 import Pages from './pages/Pages';
 
 function App() {
-  return <Pages />;
+  return (
+    <>
+      <Pages />
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
+}
+body {
+  font-family: Roboto, Arial, sans-serif;
+  background-color: #181818;
+}
+`;
