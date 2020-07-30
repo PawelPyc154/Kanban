f59c4da5d6a9bf6c64a621606b3ea9a31b77d174
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-07-30 15:29:05 +0200

    add login view

---
+++ src/components/pages/login/Login.tsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+import React from 'react';
+import media from '../../../utils/MediaQueries';
+
+export interface LogInProps {}
+
+const LogIn: React.FC<LogInProps> = () => (
+  <Main>
+    <Label htmlFor="login">Login</Label>
+    <Input id="login" type="text" />
+    <Label htmlFor="password">Password</Label>
+    <Input id="password" type="password" />
+    <Button type="button">Login</Button>
+  </Main>
+);
+
+export default LogIn;
+
+const Main = styled.main`
+  width: auto;
+  max-width: 400px;
+  margin: auto;
+  padding: 200px 3px;
+  display: flex;
+  flex-direction: column;
+  @media (orientation: landscape) {
+    padding-top: 50px;
+  }
+  ${media.tablet} {
+    padding: 200px 3px;
+  }
+`;
+const Input = styled.input`
+  height: 40px;
+  margin: 5px 0 20px 0;
+  font-size: 25px;
+  border: none;
+  padding: 5px;
+`;
+const Label = styled.label`
+  font-size: 20px;
+`;
+const Button = styled.button`
+  height: 40px;
+  margin: 10px 0 20px 0;
+  font-size: 30px;
+  background-color: gray;
+  border: none;
+  cursor: pointer;
+`;
